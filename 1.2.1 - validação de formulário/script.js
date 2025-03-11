b7fa901709ed6dd45c36f0215691f20029c713c1
07-mvieira <caio.vieira2807@gmail.com>
2025-03-11 11:58:37 -0300

    Initial commit

---
+++ 1.2.1 - validação de formulário/script.js
@@ -0,0 +1,13 @@
+
+function verificarNome(){
+    let nome = document.getElementById("nomeCaixa").value;
+    document.getElementById("hidden").style.display = "block";
+    document.getElementById("confirmacao").innerHTML = `você confirma que seu nome é ${nome}?`;
+};
+
+function confirma(){
+    let nome = document.getElementById("nomeCaixa").value;
+    document.getElementsByClassName("confBotao").disabled = true;
+    document.getElementById("sim").style.display = "block";
+    document.getElementById("sim").innerHTML = `entendi, o seu nome é ${nome}! lindo nome :D`;
+};
